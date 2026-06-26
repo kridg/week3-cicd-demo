55cf3e61b8cfbef1bcd269d5e023ce51a74726d1
kridg <ghalekridip67@gmail.com>
2026-06-27 00:28:06 +0545

    CI: task 1 workflow

---
+++ app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Version: 1.0.0"
+echo "Hostname: $(hostname)"
+echo "Date: $(date)"
+echo "System Info1 (OS): $(uname -o)"
+echo "System Info2 (Kernel): $(uname -r)"
+echo "System Info3 (Uptime): $(uptime -p)"
---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./app.sh
+
+# checking success
+if [ $? -eq 0 ]; then
+    echo "PASS"
+    exit 0
+else
+    echo "FAIL"
+    exit 1
+fi
